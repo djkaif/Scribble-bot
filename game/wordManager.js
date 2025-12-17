581c7b44ccb37973c12da5fcda9330f6344f398a
djkaif <moviedj14@gmail.com>
2025-12-17 10:21:10 +0600

    Update wordManager.js

--- game/wordManager.js
+++ game/wordManager.js
@@ -1,20 +1,18 @@
 import fs from "fs";
 
-// Ensure words.txt exists in the root directory
-const WORDS = fs
-  .readFileSync("words.txt", "utf8")
+const WORDS = fs.readFileSync("words.txt", "utf8")
   .split(/\r?\n/)
   .map(w => w.trim())
   .filter(Boolean);
 
 export function getRandomWord() {
-  if (!WORDS.length) return "apple";
-  return WORDS[Math.floor(Math.random() * WORDS.length)].toLowerCase();
+  return WORDS.length
+    ? WORDS[Math.floor(Math.random() * WORDS.length)].toLowerCase()
+    : "apple";
 }
 
 export function maskWord(word, revealed) {
-  return word
-    .split("")
-    .map((c, i) => (revealed.has(i) ? c : "_"))
+  return word.split("")
+    .map((c, i) => revealed.has(i) ? c : "_")
     .join(" ");
 }
